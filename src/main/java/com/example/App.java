67b00ae30ac99ae068d31729f441ace22ed7842c
Ezra Frost <efrost@eidebailly.com>
2024-09-18 17:08:04 -0600

    changed app to s simple server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,19 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  */
 public final class App {
+
     private App() {
     }
 
@@ -12,6 +22,40 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final int port = 8080;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                handleClient(clientSocket);
+            }
+        } catch (IOException e) {
+            System.err.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleClient(Socket clientSocket) {
+        try (
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            OutputStream output = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+        ) {
+            // Cient greeting
+            writer.println("Hello");
+
+            String clientMessage = reader.readLine();
+            System.out.println("received from client: " + clientMessage);
+
+            clientSocket.close();
+        } catch (IOException e) {
+            System.err.println("Client handling exception: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
